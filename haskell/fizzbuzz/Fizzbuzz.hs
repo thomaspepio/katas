a15be6eb8132d81f989d91823876d21033e2643f
Thomas Pepio <tpepio@octo.com>
2018-01-31 17:51:46 +0100

    Delete boilerplate

--- haskell/fizzbuzz/Fizzbuzz.hs
+++ haskell/fizzbuzz/Fizzbuzz.hs
@@ -2,19 +2,10 @@ module Fizzbuzz where
 
 fizzbuzz :: Int -> Either String Int
 fizzbuzz n
-    | divisibleByFifteen n = Left "Fizzbuzz"
-    | divisibleByThree n= Left "Fizz"
-    | diisibleByFive n = Left "Buzz"
+    | divisibleBy n 15 = Left "Fizzbuzz"
+    | divisibleBy n 3 = Left "Fizz"
+    | divisibleBy n 5 = Left "Buzz"
     | otherwise = Right n
-
-divisibleByThree :: Int -> Bool
-divisibleByThree n = divisibleBy n 3
-
-diisibleByFive :: Int -> Bool
-diisibleByFive n = divisibleBy n 5
-
-divisibleByFifteen :: Int -> Bool
-divisibleByFifteen n = divisibleBy n 15
 
 divisibleBy :: Int -> Int -> Bool
 divisibleBy number divisor = rem number divisor == 0
