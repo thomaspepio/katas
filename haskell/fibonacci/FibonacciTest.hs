5f08bda16dc3ab337ac0c8913b03cb0a439eb9bf
Thomas <tpepio@octo.com>
2018-12-17 15:29:46 +0100

    Add quickcheck tests to the fibonacci kata

--- haskell/fibonacci/FibonacciTest.hs
+++ haskell/fibonacci/FibonacciTest.hs
@@ -2,6 +2,7 @@ module FibonacciTest where
 
 import           Fibonacci
 import           Test.Hspec
+import           Test.QuickCheck
 
 spec :: Spec
 spec = describe "Kata : print the fibonacci numbers an do it in TDD." $ do
@@ -18,5 +19,31 @@ spec = describe "Kata : print the fibonacci numbers an do it in TDD." $ do
     it "should return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55] when asked to print ten numbers" $ do
         fibonacci 10 `shouldBe` [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+
+properties :: Spec
+properties = describe "Properties of fibonacci" $ do
+
+        it "verifies the property of the length of returned list" $ do
+            quickCheck prop_fibonacci_length_zero
+            quickCheck prop_fibonacci_length_one
+            quickCheck prop_fibonacci_length_two
+            quickCheckWith stdArgs { maxSuccess = 17 } prop_fibonacci_length_otherwise
+
+        it "verifies that the last value is the sum of it's two predecessors" $ do
+            quickCheckWith stdArgs { maxSuccess = 17 } prop_fibonacci_values
+
+
+values :: Gen Int
+values = choose (3, 20)
+
+prop_fibonacci_length_zero      = length (fibonacci 0) == 1
+prop_fibonacci_length_one       = length (fibonacci 1) == 1
+prop_fibonacci_length_two       = length (fibonacci 2) == 2
+prop_fibonacci_length_otherwise = forAll values $ \x -> length (fibonacci x) == length (fibonacci $ x - 1) + 1
+
+prop_fibonacci_values = forAll values $ \x -> (last $ fibonacci x) == (last $ init $ fibonacci x) + (last $ (init . init) (fibonacci x))
+
 main :: IO ()
-main = hspec spec
+main = do
+    hspec spec
+    hspec properties
